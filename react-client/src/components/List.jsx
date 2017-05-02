a3b570814ff20a7e87a20cb46537bdf9f7475966
Alana <alana.turangan@gmail.com>
2017-05-02 10:12:50 -0700

    Need data from API

--- react-client/src/components/List.jsx
+++ react-client/src/components/List.jsx
@@ -1,12 +1,44 @@
 import React from 'react';
 import ListItem from './ListItem.jsx';
 
-const List = (props) => (
-  <div>
-    <h4> List Component </h4>
-    There are { props.items.length } items.
-    { props.items.map(item => <ListItem item={item}/>)}
-  </div>
-)
+// var fishData = [
+//   {
+//     name: 'Goldfish',
+//     image: 'http://tinyurl.com/n4vgcl5',
+//     description: 'Everyone\'s first pet'
+//   },
+//   {
+//     name: 'Pufferfish',
+//     image: 'http://tinyurl.com/kxd7cuu',
+//     description: 'So puffy!'
+//   },
+//   {
+//     name: 'Tuna',
+//     image: 'http://tinyurl.com/zgs7z2s',
+//     description: 'Why are these things so huge? THey\'re terrifying.'
+//   }
+// ];
+
+// ReactDOM.render(
+//   <FishTable fishes={fishData}/>,
+//   document.getElementById('app')
+// );
+
+
+var List = (props) => (
+  <table>
+    <tbody> 
+      {props.items.map(function(item, index) {
+        return <ListItem item={item}/>;
+      })}
+    </tbody>
+  </table>
+);
+
+List.propTypes = {
+  items: React.PropTypes.array.isRequired
+};
 
 export default List;
+
+//window.List = List;
--- react-client/src/components/ListItem.jsx
+++ react-client/src/components/ListItem.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
-const ListItem = (props) => (
-  <div>
-    { props.item.description }
-  </div>
-)
+class ListItem extends React.Component {
+  constructor(props) {
+    super(props); 
 
+    this.state = {
+      showDescription: false
+
+      //idk what else goes here
+    };
+  }
+
+  render() {
+    return (
+      <tr onClick={() => this.setState({showDescription: !this.showDescription})}>
+        <td className="item-name">{this.props.item.name}</td>
+        {this.state.showDescription ? <td className="item.description">{this.props.item.description}</td> : null}
+      </tr>
+    )
+  }
+}
+
+ListItem.propTypes= {
+  item: React.PropTypes.object.isRequired
+}
+
+//window.listItem = listItem; 
 export default ListItem;
