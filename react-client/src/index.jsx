e34661f014b5b191b00c6268e72c4795782dc5e6
Phongtlam <Phongtlam@gmail.com>
2017-05-01 17:13:23 -0700

    -Revised starter page

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,35 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import List from './components/List.jsx';
+// import List from './components/List.jsx';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       items: []
     }
   }
 
   componentDidMount() {
-    $.ajax({
-      url: '/items', 
-      success: (data) => {
-        this.setState({
-          items: data
-        })
-      },
-      error: (err) => {
-        console.log('err', err);
-      }
-    });
   }
 
   render () {
-    return (<div>
-      <h1>Item List</h1>
-      <List items={this.state.items}/>
-    </div>)
+    return (
+      <MuiThemeProvider>
+        <div>
+
+        </div>
+      </MuiThemeProvider>
+    )
   }
 }
 
