36905024c109f2966606fa8e7184f55f04d63639
Jonathan Bradshaw <lavin@posteo.de>
2017-05-04 10:35:39 -0700

    Get geo coordinates, pass to server

--- server/index.js
+++ server/index.js
@@ -2,11 +2,14 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var items = require('../database-mongo');
 var request = require ('request');
-var yelpToken = '54robtCPOWAAru28w0M7Qr71NEaFNqygTcxM1xUlg3oX5aXjk3q85eX_MFH0o6SdddycpMcrPuYaV99yy_qAOKOVJWrudk8qnx80uxuCwAyxpgdA62d-27GZIdMIWXYx'
+var yelpToken = '54robtCPOWAAru28w0M7Qr71NEaFNqygTcxM1xUlg3oX5aXjk3q85eX_MFH0o6SdddycpMcrPuYaV99yy_qAOKOVJWrudk8qnx80uxuCwAyxpgdA62d-27GZIdMIWXYx';
+var location = require('./location.js')
 
 var app = express();
 
 app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/items', function (req, res) {
   items.selectAll(function(err, data) {
@@ -18,9 +21,13 @@ app.get('/items', function (req, res) {
   });
 });
 
+app.post('/location', (req, res) => {
+  console.log(req.body);
+  res.end();
+})
 
-app.get('/search', function (req, res) {
-  var input = "flying%20falafel"//req.query.query;
+app.get('/search', function(req, res) {
+  var input = req.query.query;
   var apiURL = 'https://api.yelp.com/v3/businesses/'
   var authHeader = {Authorization: "Bearer " + yelpToken}
   var placeObject = {
@@ -28,6 +35,10 @@ app.get('/search', function (req, res) {
     rating: 0,
     reviews: []
   };
+
+  console.log(req.query)
+  // var coords = req.body.coords;
+  // console.log("hello from index.js: ", coords);
 
   request( { headers: authHeader, uri: `${apiURL}search?term=${input}&latitude=37.7876&longitude=-122.4001&limit=1` }, (err, response, data) => {
     var businessID = JSON.parse(data).businesses[0].id;
@@ -58,6 +69,7 @@ app.get('/search', function (req, res) {
     });
   });
 });
+
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
