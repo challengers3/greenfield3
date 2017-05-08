83e9b1a7400afb92d5c074d34452980c3dd30f58
Phongtlam <Phongtlam@gmail.com>
2017-05-07 22:05:32 -0700

    Fixing linter

--- server/index.js
+++ server/index.js
@@ -1,18 +1,19 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var items = require('../database-mongo');
-var request = require ('request');
-var yelpToken = '54robtCPOWAAru28w0M7Qr71NEaFNqygTcxM1xUlg3oX5aXjk3q85eX_MFH0o6SdddycpMcrPuYaV99yy_qAOKOVJWrudk8qnx80uxuCwAyxpgdA62d-27GZIdMIWXYx'
+const express = require('express');
+const bodyParser = require('body-parser');
+const items = require('../database-mongo');
+const request = require('request');
 
-var app = express();
+const yelpToken = '54robtCPOWAAru28w0M7Qr71NEaFNqygTcxM1xUlg3oX5aXjk3q85eX_MFH0o6SdddycpMcrPuYaV99yy_qAOKOVJWrudk8qnx80uxuCwAyxpgdA62d-27GZIdMIWXYx';
+
+const app = express();
 
 // UNCOMMENT FOR REACT
-app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(express.static(`${__dirname}/../react-client/dist`));
 app.use(bodyParser.json());
 
-app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
+app.get('/items', (req, res) => {
+  items.selectAll((err, data) => {
+    if (err) {
       res.sendStatus(500);
     } else {
       res.json(data);
@@ -20,14 +21,14 @@ app.get('/items', function (req, res) {
   });
 });
 
-app.get('/search', function (req, res) {
-  request({ headers: {Authorization: "Bearer " + yelpToken}, uri: 'https://api.yelp.com/v3/businesses/search?term=popsons&latitude=37.7876&longitude=-122.4001&limit=1'}, (err, response, data) => {
-    var listing = data;
+app.get('/search', (req, res) => {
+  request({ headers: { Authorization: `Bearer ${yelpToken}` }, uri: 'https://api.yelp.com/v3/businesses/search?term=popsons&latitude=37.7876&longitude=-122.4001&limit=1' }, (err, response, data) => {
+    const listing = data;
     res.send(data);
   });
-  //res.send('GET');
+  // res.send('GET');
 });
 
-app.listen(3000, function() {
+app.listen(3000, () => {
   console.log('listening on port 3000!');
 });
