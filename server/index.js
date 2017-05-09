d7ff641335212f2fd873a67f9fa3ea8581e4cc46
Jonathan Bradshaw <lavin@posteo.de>
2017-05-08 17:00:40 -0700

    Get all data from Yelp

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var items = require('../database-mongo');
 var request = require ('request');
+var axios = require('axios');
 var yelpToken = '54robtCPOWAAru28w0M7Qr71NEaFNqygTcxM1xUlg3oX5aXjk3q85eX_MFH0o6SdddycpMcrPuYaV99yy_qAOKOVJWrudk8qnx80uxuCwAyxpgdA62d-27GZIdMIWXYx';
 var location = {};
+
 
 var app = express();
 
@@ -27,45 +29,74 @@ app.post('/location', (req, res) => {
 })
 
 app.get('/search', function(req, res) {
-  var input = req.query.query;
-  var apiURL = 'https://api.yelp.com/v3/businesses/'
-  var authHeader = {Authorization: "Bearer " + yelpToken}
-  var placeObject = {
+  let input = req.query.query;
+  let apiURL = 'https://api.yelp.com/v3/businesses/'
+  let authHeader = {Authorization: "Bearer " + yelpToken}
+  let localeObject = {
     name: '',
+    address: '',
+    phone: '',
+    type: '',
+    price: '',
+    photos: [],
     rating: 0,
-    reviews: []
+    reviews: [],
+    url: ''
   };
-  var userLat = location.lat;
-  var userLong = location.long;
-  console.log(location)
+  let userLat = 37.7836964//location.lat;
+  let userLong = -122.40916799999998;//location.long;
 
-  request( { headers: authHeader, uri: `${apiURL}search?term=${input}&latitude=${userLat}&longitude=${userLong}&limit=1` }, (err, response, data) => {
-    var businessID = JSON.parse(data).businesses[0].id;
+  console.log(userLat, userLong)
 
-    request( { headers: authHeader,uri:`${apiURL}${businessID}` }, (err, response, placeData) => {
-      var placeData = JSON.parse(placeData);
+  let getBusinessData = (businessID) => {
+    axios({
+      method: 'get',
+      headers: authHeader,
+      url: `${apiURL}${businessID}`
+    }).then( yelpBizData => {
+      let localeData = yelpBizData.data;
 
-      placeObject.name = placeData.name;
-      placeObject.rating = placeData.rating;
-    });
+      localeObject.name = localeData.name;
+      localeObject.address = localeData.location.address1;
+      localeObject.cross = localeData.location.cross_streets;
+      localeObject.phone = localeData.display_phone;
+      localeObject.rating = localeData.rating;
+      localeObject.reviewCount = localeData.review_count;
+      localeObject.type = localeData.categories[0].title;
+      localeObject.price = localeData.price;
+      localeObject.photos = localeData.photos;
+      localeObject.url = localeData.url;
+    }).catch( err => console.log('baseLocaleError: ', err));
+  }
 
-    request( { headers: authHeader,uri:`${apiURL}${businessID}/reviews` }, (err, response, reviewData) => {
-      var reviewData = JSON.parse(reviewData).reviews;
+  let getBusinessReviews = (businessID) => {
+    axios({
+      method: 'get',
+      headers: authHeader,
+      url: `${apiURL}${businessID}/reviews`
+    }).then( yelpBizData => {
+      let localeData = yelpBizData.data.reviews;
+        for(var i = 0; i < 3; i++) {
+          localeObject.reviews.push({
+            'text': localeData[i].text,
+            'rating': localeData[i].rating,
+            'reviewer_name': localeData[i].user.name,
+            'url': localeData[i].url
+          });
+        }
+    }).catch( err => console.log('businessReviewError: ', err));
+  }
 
-      for(var i = 0; i < reviewData.length; i++) {
-        placeObject.reviews.push({
-          'text': reviewData[i].text,
-          'rating': reviewData[i].rating,
-          'reviewer_name': reviewData[i].user.name,
-          'url': reviewData[i].url
-        });
-      }
-      // save to the DB at this point
-      // another comment
-
-      res.send(placeObject);
-    });
-  });
+  axios({
+    method: 'get',
+    headers: authHeader,
+    url: `${apiURL}search?term=${input}&latitude=${userLat}&longitude=${userLong}&limit=1`
+  }).then( yelpData => yelpData.data.businesses[0].id)
+  .then( businessID => {
+    getBusinessData(businessID);
+    getBusinessReviews(businessID);
+    setTimeout( () => res.send(localeObject), 2000 );
+  }).catch( err => console.log('promise error: ', err));
 });
 
 
