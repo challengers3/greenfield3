5b834a76fb9ba1e600637a140e6433cb99f2a79e
Jonathan Bradshaw <lavin@posteo.de>
2017-05-14 23:17:16 -0700

    Updating for merge

--- react-client/src/App.jsx
+++ react-client/src/App.jsx
@@ -117,7 +117,7 @@ class App extends React.Component {
     });
   }
 
-  // this is to feed item onto state.delItem from child
+  // feed item onto state.delItem from child
   speechRemoveHandler(data) {
     this.setState({ delItem: data });
   }
